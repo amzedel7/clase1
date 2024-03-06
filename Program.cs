3c73e40a504bf30052e4ea910560d72c478706c0
Elias Rojas <amzedel7@gmail.com>
2024-03-05 20:31:56 -0400

    c

---
+++ Program.cs
@@ -0,0 +1,8 @@
+﻿// See https://aka.ms/new-console-template for more information
+using system;
+static void Main()
+{
+Console.WriteLine("HOLA, MUNDO!");
+Console.WriteLine("materia dificil");
+}
+
